cec60d0a6dc9685415e3a78b39f11f0f25419943
doyousketch2 <doyousketch2@yahoo.com>
2018-10-04 18:11:17 -0400

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,12 +1,14 @@
 
 print( '[afk]  CSM loading...' )
 
-local player1name  = ''
+afk  = false
+playername  = ''
+awaymessage  = "I'm AFK, try again later."
 
+--  https://raw.githubusercontent.com/minetest/minetest/master/doc/client_lua_api.txt
 --~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 local function show_main_dialog()
-
   local formspec  = 'size[6,8]'
     ..'bgcolor[#080808BB]'
     ..'label[0.1,0;Away From Keyboard]'
@@ -20,10 +22,24 @@ end
 minetest .register_on_receiving_chat_messages(
   function(message)
 
-    local msg  = minetest .strip_colors(message)
+  local msg  = minetest .strip_colors(message)
 
-    if msg :find( player1name ) then
-      minetest .send_chat_message( "I'm AFK, try again later." )
+    if afk and msg :find( playername ) > 0 then
+      print( '[afk] found playername' )
+      minetest .send_chat_message( awaymessage )
+    end
+
+  end
+)
+
+--~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+minetest .register_on_formspec_input(
+  function( formname, fields )
+    if formname ~= 'afk:menu' then return false
+
+    elseif fields .close then
+      afk  = false
     end
 
   end
@@ -32,21 +48,22 @@ minetest .register_on_receiving_chat_messages(
 --~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 minetest .register_on_connect(
-  function() -- 1
+  function() -- outer
     -- delay a moment for Minetest to initialize
-    minetest .after( 7,
-      function() -- 2
-        player1name  = minetest .localplayer :get_name()
+    minetest .after( 5,
+      function() -- inner
+        playername  = minetest .localplayer :get_name()
+        print( '[afk] playername ||' ..playername ..'||' )
 
         local M1  = minetest .colorize( '#BBBBBB', '[afk] CSM loaded, type ' )
         local M2  = minetest .colorize( '#FFFFFF', '.afk ' )
         local M3  = minetest .colorize( '#BBBBBB', "when you're Away From Keyboard" )
         minetest .display_chat_message( M1..M2..M3 )
 
-      end  -- function() -- 2
-    )  -- .after(7)
+      end  -- function() -- inner
+    )  -- .after(5)
 
-  end  -- function() -- 1
+  end  -- function() -- outer
 )  -- .register_on_connect()
 
 --~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -54,7 +71,7 @@ minetest .register_on_connect(
 minetest .register_chatcommand( 'afk',
   {
     func  = function(param)
-      shown  = true
+      afk  = true
       show_main_dialog()
     end,
   }
