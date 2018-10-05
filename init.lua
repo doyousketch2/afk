de52eabee2edbc2fca41c706dc97d03a9a13370f
doyousketch2 <doyousketch2@yahoo.com>
2018-10-04 23:10:39 -0400

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,32 +1,65 @@
 
 print( '[afk]  CSM loading...' )
 
-local awaymessage  = "I'm AFK, try again later."
+--~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+--  "message in quotes",  followed by a comma
+
+local awaymessages  = {
+  "brb",
+  "bbiab",
+  "just a sec...",
+  "be right back",
+  "currently afk.",
+  "away for a few.",
+  "be back in a bit",
+  "hang on a minute...",
+  "away from keyboard.",
+  "afk, try again later.",
+  "afk, please try again later.",
+  "not at my keyboard right now.",
+  "please leave a message at the beep...",
+}
+
+--~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 local playername  = ''
 local nicknames  = {}
+local nickname  = ''
 local pings  = {}
 local afk  = false
+local index  = ''
+local selected  = ''
+local prev_random  = ''
+
+--~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 --  https://raw.githubusercontent.com/minetest/minetest/stable-0.4/doc/client_lua_api.md
 
 --  https://raw.githubusercontent.com/minetest/minetest/master/doc/client_lua_api.txt
+
 --~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+math .randomseed( minetest .get_us_time() )
+math.random(); math.random(); math.random()
+
 local function show_main_dialog()
-  local formspec  = 'size[8,8]'
-    ..'bgcolor[#080808BB]'
-    ..'label[0.1,0;Away From Keyboard]'
-    ..'button_exit[5.8,0.2;2.2,0.2;close;Close]'
+  local formspec  = 'size[12,8]'
+    ..'bgcolor[#080808CC]'
+
+    ..'field[0.3,0.8;8.2,0.2;nick;add nickname;]'
+    ..'button_exit[9,0.2;2.2,0.2;close;Close]'
 
     ..'tableoptions[color=#000000;background=#314D4F]'
     ..'tablecolumns[text]'
-    ..'table[0,1;7.8,2.5;nick_list;'
+    ..'table[0,1.3;8,2.5;nick_list;'
   formspec  = formspec ..table .concat( nicknames, ',' ) ..';]'
 
+    ..'button[9,2.5;2.2,0.2;remove;Remove]'
+
     ..'tableoptions[color=#000000;background=#314D4F]'
     ..'tablecolumns[text]'
-    ..'table[0,4;7.8,3.8;ping_list;'
+    ..'table[0,4;11.8,3.8;ping_list;'
   formspec  = formspec ..table .concat( pings, ',' ) ..';]'
 
   minetest .show_formspec(  'afk:menu',  formspec  )
@@ -38,19 +71,28 @@ minetest .register_on_receiving_chat_messages(
   function(message)
 
     local msg  = minetest .strip_colors(message)
+    local found  = false
 
     for nn = 1, #nicknames do
-      if afk and msg :find( nicknames[nn] ) then
+      if afk and not found and msg :find( nicknames[nn] ) then
 
         if msg :sub( 1, 1 ) == '<' then  --  normal message
           local sender  = msg :sub( 2,  msg :find( '>' ) -1 )
 
           if sender ~= playername then
-            print( '[afk] msg from: ' ..sender )
-            table.insert( pings, sender )
+            print( '[afk] msg: ' ..msg )
+            table.insert( pings, msg )
 
+            local random  = math.random( #awaymessages )
+            while random == prev_random do
+              random  = math.random( #awaymessages )
+            end
+            local awaymessage  = awaymessages[ random ]
             minetest .send_chat_message( awaymessage )
+            prev_random = random
+
             show_main_dialog()
+            found  = true
           end
 
         -- elseif  'player did some action'  then...
@@ -69,8 +111,37 @@ minetest .register_on_formspec_input(
     if formname ~= 'afk:menu' then return false
 
     elseif fields .close then
+      selected  = ''
       afk  = false
       print('[afk] CSM: returned')
+
+    elseif fields .nick_list then  -- figure out which name was selected
+      local sel  = fields .nick_list  -- CHG:row:col for singleclick  or  DBL:row:col for doubleclick
+      local click  = sel :sub(1,3)
+      if click == 'CHG' or click == 'DBL' then  -- example, when you change the field, it returns  CHG:12:3
+        -- get # from first ':'  @ pos 5.   possibility of 2 digits require you to find() the second ':'
+        local ind  = sel :sub( 5,  sel :find(':', 6) -1 )
+        index  = tonumber( ind )
+
+        selected  = nicknames[ index ]
+        show_main_dialog()
+      end  -- if click ==
+
+    elseif fields .key_enter_field then
+      local newname  = fields .nick
+      print( newname )
+      table.insert( nicknames,  newname )
+      print( '[afk] CSM added: ' ..newname )
+
+      minetest .after(  0.1,  function() show_main_dialog() end  )
+
+    elseif fields .remove and index > 1 then
+        table.remove( nicknames,  index )
+        print( '[afk] CSM removed: ' ..selected )
+
+        index  = index -1
+        selected  = nicknames[ index ]
+        show_main_dialog()
     end
 
   end  -- function( formname, fields )
@@ -117,7 +188,7 @@ minetest .register_on_connect(
         minetest .display_chat_message( M1..M2..M3 )
 
       end  -- function() -- inner
-    )  -- .after(1)
+    )  -- .after(3)
 
   end  -- function() -- outer
 )  -- .register_on_connect()
